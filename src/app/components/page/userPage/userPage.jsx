72432421c69e4cef2fa1677810c0790c532d69ff
wallnnut <81454917+wallnnut@users.noreply.github.com>
2022-12-04 12:37:16 +0300

    reactUi

--- src/app/components/page/userPage/userPage.jsx
+++ src/app/components/page/userPage/userPage.jsx
@@ -1,31 +1,28 @@
 import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import api from "../../../api";
-import Qualities from "../../ui/qualities";
-import { useHistory } from "react-router-dom";
-
+import UserCard from "../../ui/userCard";
+import QualitiesCard from "../../ui/qualitiesCard";
+import MeetingsCard from "../../ui/meetingsCard";
+import Comments from "../../ui/comments";
 const UserPage = ({ userId }) => {
-    const history = useHistory();
     const [user, setUser] = useState();
     useEffect(() => {
         api.users.getById(userId).then((data) => setUser(data));
     }, []);
-    const handleClick = () => {
-        history.push(`/users/${userId}/edit`);
-    };
     if (user) {
         return (
-            <div>
-                <h1> {user.name}</h1>
-                <h6> Email: {user.email}</h6>
-                <h2>Профессия: {user.profession.name}</h2>
-                <h6>Пол: {user.sex}</h6>
-                <Qualities qualities={user.qualities} />
-                <p>completedMeetings: {user.completedMeetings}</p>
-                <h2>Rate: {user.rate}</h2>
-                <button onClick={handleClick}>
-                    Редактировать пользователя
-                </button>
+            <div className="container">
+                <div className="row gutters-sm">
+                    <div className="col-md-4 mb-3">
+                        <UserCard user={user} />
+                        <QualitiesCard data={user.qualities} />
+                        <MeetingsCard value={user.completedMeetings} />
+                    </div>
+                    <div className="col-md-8">
+                        <Comments />
+                    </div>
+                </div>
             </div>
         );
     } else {
---
+++ src/app/components/ui/qualitiesCard.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Qualities from "./qualities";
+import Proptypes from "prop-types";
+const QualitiesCard = ({ data }) => {
+    return (
+        <div className="card mb-3">
+            <div className="card-body d-flex flex-column justify-content-center text-center">
+                <h5 className="card-title">
+                    <span>Qualities</span>
+                </h5>
+                <p className="card-text">
+                    <Qualities qualities={data} />
+                </p>
+            </div>
+        </div>
+    );
+};
+
+QualitiesCard.propTypes = {
+    data: Proptypes.array
+};
+
+export default QualitiesCard;
